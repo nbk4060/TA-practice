be0d6cd399e922e1a30a9edc60c9ee5990131e26
nbk4060 <noor.alphasquad@gmail.com>
2024-12-21 04:51:02 +0500

    minor fix

--- src/components/reusableComponents/Button.js
+++ src/components/reusableComponents/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ text, className, icon, onclick }) => {
   return (
-    <button className={`${className}`} onClick={onclick}>
+    <button className={`${className} py-[9px]`} onClick={onclick}>
       <span>{text}</span>
       {icon && <span><img src={icon} alt={text}/></span>}
     </button>
